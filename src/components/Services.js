4e6fa2e533f24d7fee06a716521758b253284a9a
RIDHA OMRI <omriridha@hotmail.com>
2019-06-08 16:17:11 +0200

    services development

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react'
 import Title from "../components/Title"
+import { FaCocktail, FaHiking, FaBeer, FaShuttleVan } from "react-icons/fa"
 export default class Services extends Component {
+    state = {
+        services:
+            [
+                {
+                    icon: <FaCocktail/>,
+                    title: "free Cocktails",
+                    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ne infantes quidem possin"
+
+
+                },
+                {
+                    icon: <FaHiking/>,
+                    title: "Endless Hiking",
+                    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ne infantes quidem possin"
+
+
+                },
+                {
+                    icon: <FaShuttleVan />,
+                    title: "Free Shuttle",
+                    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ne infantes quidem possin"
+
+
+                },
+                {
+                    icon: <FaBeer />,
+                    title: "free Beer",
+                    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ne infantes quidem possin"
+
+
+                }
+            ]
+    }
     render() {
         return (
-            <div>
+            <section className="services">
+
                 <Title title="services" />
-            </div>
-        )
-    }
-}
+                <div className="services-center">
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                            }) 
+                          }
+                </div>
+       
 
-
-
-
-
+            </section>
+                
+            
+                )
+            }
+        }
+        
+        
+        
+        
+        
