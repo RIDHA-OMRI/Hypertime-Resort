5a5c2413a8a77b35f87679603aa3551c8faf51fd
RIDHA OMRI <omriridha@hotmail.com>
2019-06-08 15:30:10 +0200

    Update version with new services

---
+++ src/components/Services.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Title from "../components/Title"
+export default class Services extends Component {
+    render() {
+        return (
+            <div>
+                <Title title="services" />
+            </div>
+        )
+    }
+}
+
+
+
+
+
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                Hello world from Single room
+            </div>
+        )
+    }
+}
+
+
